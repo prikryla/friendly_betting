1d7c5a330fb5ebf3fde25725972624bf7447df3b
Jan Mrákava <66779580+janmrakava@users.noreply.github.com>
2024-03-04 21:37:34 +0100

    add basic structure of landing page, create login route

--- frontend/src/app/components/Logo/logo.tsx
+++ frontend/src/app/components/Logo/logo.tsx
@@ -2,12 +2,12 @@ import Image from "next/image";
 
 const Logo = () => {
   return (
-    <div className="flex items-center">
-      <h1>
+    <div className="flex gap-7">
+      <Image src="/Logo.png" alt="Logo of app" width={100} height={100} />
+      <h1 className="text-4xl md:text-5xl">
         Friendly <br />
         Betting
       </h1>
-      <Image src="/Logo.png" alt="Logo of app" width={200} height={146} />
     </div>
   );
 };
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,12 +1,22 @@
 import styles from "./page.module.css";
 import Logo from "./components/Logo/logo";
 import "tailwindcss/tailwind.css";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <Logo />
-      <h1>Login</h1>
+    <main className="flex justify-between items-center h-screen">
+      <Image
+        src="/landingimage.png"
+        alt="Image of Soccer Stadion with Ball"
+        width={1004}
+        height={1117}
+        className="w-1/2 opacity-90"
+      />
+      <div className="w-1/2 flex-row items-center justify-center border">
+        <Logo />
+        <p>Vitejte v nové aplikaci</p>
+      </div>
     </main>
   );
 }
