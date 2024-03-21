6ba13f5490c5274ecd959506eed2d9327b782d8a
Jan Mrákava <66779580+janmrakava@users.noreply.github.com>
2024-03-21 21:45:48 +0100

    fix route to BE

--- frontend/auth.ts
+++ frontend/auth.ts
@@ -8,7 +8,7 @@ import axios from 'axios';
 async function loginUser(email: string, password: string) {  
     console.log("loginUser metoda")
     try {
-        const response = await axios.post('127.0.0.1:8000/api/auth/login', {email, password});
+        const response = await axios.post('http://127.0.0.1:8000/api/auth/login/', {email, password});
         return response.data;
     } catch (error) {
         console.log(error)
