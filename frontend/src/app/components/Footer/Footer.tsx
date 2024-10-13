2961d892778441e9c9664fedfff52625836b5570
Jan Mrákava <66779580+janmrakava@users.noreply.github.com>
2024-10-13 21:24:39 +0200

    add footer component

---
+++ frontend/src/app/components/Footer/Footer.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import FooterLogo from './FooterLogo/FooterLogo';
+
+import './Footer.scss';
+
+
+const Footer: React.FC = () => {
+    const date = new Date();
+    const renderDate = 2024 === date.getFullYear() ? 2024 : `2024-${date.getFullYear()}`;
+    
+    return (
+        <main className='footerContainer'>
+            <FooterLogo />
+            <p className='randomText'>{date.getDate()}.{date.getMonth()+1}.{date.getFullYear()} {date.getHours()}:{date.getMinutes()}</p>
+            <p className='authors'>
+                <span>&#169;  {renderDate} </span> 
+                <a href='https://www.linkedin.com/in/adam-prikryl/' className='link' >Adam Přikryl</a>
+                <span>  &  </span>
+                <a href='https://www.linkedin.com/in/janmrakava/' className='link' >Jan Mrákava</a>
+            </p>
+        </main>
+    )
+}
+
+export default Footer;
---
+++ frontend/src/app/components/Footer/FooterLogo/FooterLogo.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+import './FooterLogo.scss';
+import Image from "next/image";
+
+import './FooterLogo.scss';
+
+
+const FooterLogo: React.FC = () => {
+    return (
+        <main className="footerLogo">
+            <Image src='/Logo.png' alt='Logo aplikace' width={70} height={58} />
+            <h1 className="footerLogoText">Friendly Betting</h1>
+        </main>
+    )
+}
+
+export default FooterLogo;
