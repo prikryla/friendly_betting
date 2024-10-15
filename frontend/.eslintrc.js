aab8f1b2b21d8c1e0f2f7e73e58d85d4a31748d7
Jan Mrákava <66779580+janmrakava@users.noreply.github.com>
2024-10-15 21:59:48 +0200

    configure eslint & prettier

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,69 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "standard-with-typescript",
+        "plugin:react/recommended",
+        "plugin:react-hooks/recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:prettier/recommended",
+        "eslint:recommended",
+        "prettier",
+    ],
+    "ignorePatterns": ["dist", ".eslintrc.cjs", "prettier.config.cjs"],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module",
+        "project": "./tsconfig.json",
+        "tsconfigRootDir": __dirname,
+        "ecmaFeatures": {
+            "jsx": true
+        }
+    },
+    "plugins": [
+        "react",
+        "@typescript-eslint",
+        "react-hooks",
+        "import",
+        "react-refresh",
+        "prettier"
+    ],
+    "rules": {
+        // Maximum line length
+        "max-len": ["error", { "code": 128 }],
+
+        // Disallow console.log
+        "no-console": ["error", { "allow": ["warn", "error"] }],
+
+        // Disallow more than one empty line
+        "no-multiple-empty-lines": ["error", { "max": 1 }],
+
+        // React and hooks rules
+        "react/jsx-uses-react": "off",
+        "react/react-in-jsx-scope": "off",
+        "react/prop-types": "off",
+        "react-hooks/rules-of-hooks": "error",
+        "react-hooks/exhaustive-deps": "warn",
+
+        // TypeScript rules
+        "@typescript-eslint/no-unused-vars": ["warn", { "argsIgnorePattern": "^_" }],
+        "@typescript-eslint/no-explicit-any": "warn",
+        
+        // Prettier rules
+        "prettier/prettier": ["error", {
+            "singleQuote": true,
+            "trailingComma": "all",
+            "jsxSingleQuote": true,
+            "printWidth": 100
+        }]
+    },
+
+    "settings": {
+        "react": {
+            "version": "detect"
+        }
+    }
+}
---
+++ frontend/.prettierrc.js .js
@@ -0,0 +1,9 @@
+module.exports = {
+  trailingComma: 'all',  // Always use trailing commas
+  printWidth: 100,  // Line width of 100 characters
+  semi: true,  // Use semicolons
+  singleQuote: true,  // Use single quotes
+  jsxSingleQuote: true,  // Use single quotes in JSX
+  tabWidth: 2,  // Tab width of 2 spaces
+  quoteProps: 'as-needed'  // Use quotes around object properties only when necessary
+};
