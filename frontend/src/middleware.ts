768529057473fade3349e7bf9f34155ef3571b48
Jan Mrákava <66779580+janmrakava@users.noreply.github.com>
2024-10-20 12:24:33 +0200

    prettier eslint

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -1,23 +1,27 @@
 import NextAuth from 'next-auth';
-import { NextResponse } from "next/server";
+import { NextResponse } from 'next/server';
 import { authConfig } from '../auth.config';
 
-const {auth} = NextAuth(authConfig);
+const { auth } = NextAuth(authConfig);
 
 const authRoutes = ['/dashboard'];
 
 export default auth((req) => {
     const { nextUrl } = req;
     const isLoggedIn = !!req.auth;
-    const isApiAuthRoute = nextUrl.pathname.startsWith('/login') || nextUrl.pathname === '/' || nextUrl.pathname === '/register' || nextUrl.pathname === '/forgotpassword';
+    const isApiAuthRoute =
+        nextUrl.pathname.startsWith('/login') ||
+        nextUrl.pathname === '/' ||
+        nextUrl.pathname === '/register' ||
+        nextUrl.pathname === '/forgotpassword';
     const isPublicRoute = nextUrl.pathname === '/';
-        
-    const isAuthRoute = authRoutes.some(route => nextUrl.pathname.startsWith(route));
-    
+
+    const isAuthRoute = authRoutes.some((route) => nextUrl.pathname.startsWith(route));
+
     if (isApiAuthRoute) {
         return NextResponse.next();
-    } 
-    
+    }
+
     if (isAuthRoute) {
         if (!isLoggedIn) {
             return NextResponse.redirect(new URL('/login', nextUrl));
